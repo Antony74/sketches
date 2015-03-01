d2ffc8709c49400ea8ee7adcc83be187348d03df
Antony74 <akb@akb.me.uk>
2015-03-01 18:14:46 +0000

    Place the two slits.

--- QuantumInterference/main.js
+++ QuantumInterference/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function()
 {
     var sNormFn = $('#NormFn').val();
+    var sLeftFn = $('#LeftFn').val();
+    var sRightFn = $('#RightFn').val();
 
     $('#prettyNormFn').html('$$' + math.parse(sNormFn).toTex() + '$$');
 
@@ -9,19 +11,16 @@ $(document).ready(function()
         sCanvasID: 'canvasPlot',
         width:     640,
         height:    300,
-        xmin:       -2,
-        ymin:    - 0.1,
-        xmax:        2,
+        xmin:       -4,
+        ymin:     -0.1,
+        xmax:        4,
         ymax:      0.5,
     });
 
-    var scope = 
-    {
-        mu:    0,
-        sigma: 1,
-    };
-
-    var thing = math.eval(sNormFn, scope);
-    graph.plot(thing);
+    var normFn = math.eval(sNormFn);
+    var leftFn = math.eval(sLeftFn,   {Norm: normFn});
+    var rightFn = math.eval(sRightFn, {Norm: normFn});
+    graph.plot(leftFn);
+    graph.plot(rightFn);
 });
 
